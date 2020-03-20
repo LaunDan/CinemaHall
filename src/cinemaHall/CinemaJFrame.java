2c337c981ce3485f413634e9a265bb98ff08cfdb
LaunDan <launadaniel2@gmail.com>
2020-03-20 15:09:21 +0100

    created JPanel and JButton save

--- src/cinemaHall/CinemaJFrame.java
+++ src/cinemaHall/CinemaJFrame.java
@@ -1,4 +1,9 @@
 package cinemaHall;
 
+import javax.swing.*;
+
 public class CinemaJFrame {
+    private JPanel RecordsOfCinemaHall;
+    private JPanel canvasJPanel;
+    private JButton saveJButton;
 }
