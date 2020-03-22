76eeb45cb15df6fa21fe86f0e4bb9a3dbb843d6f
LaunDan <launadaniel2@gmail.com>
2020-03-22 20:36:32 +0100

    tryin to set visible

--- src/cinemaHall/CinemaJFrame.java
+++ src/cinemaHall/CinemaJFrame.java
@@ -1,11 +1,27 @@
 package cinemaHall;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class CinemaJFrame {
+public class CinemaJFrame extends JFrame {
     private JPanel RecordsOfCinemaHall;
     private JPanel canvasJPanel;
     private JButton saveJButton;
 
     private CinemaHall cinemaHal = new CinemaHall();
+
+
+    public static void main(String[] args) {
+
+        CinemaJFrame cinemaJFrame = new CinemaJFrame();
+        cinemaJFrame.setLocationRelativeTo(null);
+        cinemaJFrame.setVisible(true);
+        cinemaJFrame.setSize(500, 500);
+
+    }
+
+    public CinemaJFrame(){
+        RecordsOfCinemaHall.setVisible(true);
+        canvasJPanel.setVisible(true);
+    }
 }
