40b01052d1ddc2be17ee49a03923925ff1c797d5
LaunDan <launadaniel2@gmail.com>
2020-03-22 19:59:06 +0100

    Set cinemaJFrame

--- src/cinemaHall/CinemaJFrame.java
+++ src/cinemaHall/CinemaJFrame.java
@@ -6,4 +6,6 @@ public class CinemaJFrame {
     private JPanel RecordsOfCinemaHall;
     private JPanel canvasJPanel;
     private JButton saveJButton;
+
+    private CinemaHall cinemaHal = new CinemaHall();
 }
