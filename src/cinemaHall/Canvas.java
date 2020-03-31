9b81c8ab0ac028a537cecfb719cf7eb4c320c269
LaunDan <launadaniel2@gmail.com>
2020-03-31 21:27:03 +0200

    set prefered size

--- src/cinemaHall/Canvas.java
+++ src/cinemaHall/Canvas.java
@@ -9,13 +9,15 @@ public class Canvas extends JPanel {
     public Canvas(CinemaHall cinemaHal) {
         super();
         this.setBackground(Color.WHITE);
-        this.setPreferredSize(new Dimension(400, 300));
+        this.setPreferredSize(new Dimension(600, 300));
         this.cinemaHa = cinemaHal;
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        int width = this.getWidth();
+        int height = this.getHeight();
         cinemaHa.printing(g);
     }
 }
